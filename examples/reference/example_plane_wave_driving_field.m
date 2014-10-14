6ceac02018a46594b9ef54e0ac1bd3e90ffe82fb
Maximilian.Hoegner <Maximilian.Hoegner@gar-ws-lapu02.garching.physik.uni-muenchen.de>
2014-10-14 14:09:07 +0200

    add example for plane_wave_driving_field

---
+++ examples/reference/example_plane_wave_driving_field.m
@@ -0,0 +1,29 @@
+% for executing, copy to main folder or use addpath
+
+% basic configuration for plane_wave_driving_field
+config = struct();
+config.wavelength = 1.0e-3; % mm
+config.peak_intensity = 7e13; % W/cm^2
+
+% set pulse coefficients (temporal shape)
+ % configure time axis (t is in scaled atomic units, i.e. one
+ % driving field period equates to 2*pi
+ time_steps = 200; % per period
+ t = -10*2*pi : 2*pi/time_steps : 10*2*pi; % 20 periods
+
+ % and get spectrum using the pulse.m module
+ config.pulse_shape = 'gaussian';
+ config.pulse_duration = 20; % fs
+ [pulse_omega, pulse_coefficients] = pulse(t, config);
+
+config.omega = pulse_omega;
+config.pulse_coefficients = pulse_coefficients;
+
+% compute on-axis driving field at origin and plot it
+% (output is in co-moving coordinates)
+Et_cmc = plane_wave_driving_field(0, 0, 0, config);
+
+plot(t, real(Et_cmc))
+title('driving field at origin')
+xlabel('time in co-moving coordinates [scaled atomic units]')
+ylabel('driving field amplitude [s.a.u.]')
