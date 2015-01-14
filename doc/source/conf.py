26461ed908d6e19039bdc58c08214243da610faf
Maximilian.Hoegner <Maximilian.Hoegner@gar-ws-lapu02.garching.physik.uni-muenchen.de>
2015-01-14 10:33:48 +0100

    update project name

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# MHHG documentation build configuration file, created by
+# HHGmax documentation build configuration file, created by
 # sphinx-quickstart on Mon Nov  4 19:44:04 2013.
 #
 # This file is execfile()d with the current directory set to its containing dir.
@@ -42,8 +42,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'MHHG'
-copyright = u'2013, Maximilian Högner'
+project = u'HHGmax'
+copyright = u'2013-2015, Maximilian Högner, LMU München'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -168,7 +168,7 @@ html_show_copyright = False
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'MHHGdoc'
+htmlhelp_basename = 'HHGmaxdoc'
 
 
 # -- Options for LaTeX output --------------------------------------------------
@@ -187,7 +187,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'MHHG.tex', u'MHHG Documentation',
+  ('index', 'HHGmax.tex', u'HHGmax Documentation',
    u'Maximilian Högner', 'manual'),
 ]
 
@@ -217,7 +217,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'mhhg', u'MHHG Documentation',
+    ('index', 'hhgmax', u'HHGmax Documentation',
      [u'Maximilian Högner'], 1)
 ]
 
@@ -231,8 +231,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'MHHG', u'MHHG Documentation',
-   u'Maximilian Högner', 'MHHG', 'One line description of project.',
+  ('index', 'HHGmax', u'HHGmax Documentation',
+   u'Maximilian Högner', 'HHGmax', 'One line description of project.',
    'Miscellaneous'),
 ]
 
--- doc/source/index.rst
+++ doc/source/index.rst
@@ -1,4 +1,4 @@
-.. MHHG documentation master file, created by
+.. HHGmax documentation master file, created by
    sphinx-quickstart on Mon Nov  4 19:44:04 2013.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
